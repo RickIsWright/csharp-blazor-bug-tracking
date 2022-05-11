efc4f220056e6cdbb497ede45e216456bbe47064
Rick Wright <rickiswright@live.com>
2022-05-11 08:48:19 -0700

    Working with services and data
    Navmanager and redirect onvalidsubmit handling.

--- BugTrackerUI/Startup.cs
+++ BugTrackerUI/Startup.cs
@@ -28,6 +28,8 @@ namespace BugTrackerUI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            services.AddSingleton<IBugService, BugService>(); // register bugservice for DI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
